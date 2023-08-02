52184367e0dc5852ad6e9c7c81c05d9200014c8c
yueguang <yueguang@qingmutec.com>
2023-08-02 23:53:47 +0800

    微服务 虚仓管理

--- oms_commodity/src/main/java/com/oms/saas/commodity/service/impl/Warehouse/WmsSimulationStoreInfoServiceImpl.java
+++ oms_commodity/src/main/java/com/oms/saas/commodity/service/impl/Warehouse/WmsSimulationStoreInfoServiceImpl.java
@@ -1,10 +1,14 @@
 package com.oms.saas.commodity.service.impl.Warehouse;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.oms.saas.commodity.Entity.Warehouse.WmsRealStoreInfo;
 import com.oms.saas.commodity.Entity.Warehouse.WmsSimulationStoreInfo;
 import com.oms.saas.commodity.Vo.Warehouse.WmsSimulationStoreInfoVO;
+import com.oms.saas.commodity.dto.JwtInfo;
 import com.oms.saas.commodity.mapper.Warehouse.WmsSimulationStoreInfoMapper;
 import com.oms.saas.commodity.service.Warehouse.WmsSimulationStoreInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,8 +22,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class WmsSimulationStoreInfoServiceImpl extends ServiceImpl<WmsSimulationStoreInfoMapper, WmsSimulationStoreInfo> implements WmsSimulationStoreInfoService {
 
+    @Resource
+    private JwtInfo jwtInfo;
+
     @Override
     public boolean save(WmsSimulationStoreInfoVO vo) {
-        return false;
+        WmsSimulationStoreInfo wmsSimulationStoreInfo = new WmsSimulationStoreInfo();
+        BeanUtil.copyProperties(vo,wmsSimulationStoreInfo);
+        wmsSimulationStoreInfo.setCompanyCode(jwtInfo.getCompanyCode());
+        return this.save(wmsSimulationStoreInfo);
     }
 }
