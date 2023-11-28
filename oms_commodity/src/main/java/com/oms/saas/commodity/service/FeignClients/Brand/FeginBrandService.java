ca1846554dc6a3d0d398a02a703398e3aa2f05d7
yueguang <yueguang@qingmutec.com>
2023-11-28 20:04:07 +0800

    微服务 OpenFegin 接入

--- oms_commodity/src/main/java/com/oms/saas/commodity/service/FeignClients/Brand/FeginBrandService.java
+++ oms_commodity/src/main/java/com/oms/saas/commodity/service/FeignClients/Brand/FeginBrandService.java
@@ -2,6 +2,7 @@ package com.oms.saas.commodity.service.FeignClients.Brand;
 
 import com.oms.saas.commodity.api.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,4 +12,7 @@ public interface FeginBrandService {
 
     @PostMapping(value = "/brandInfo/getBrandByUserId")
     public Result getBrandByUserId(@RequestHeader("token") String token, @RequestParam("userId") Integer userId);
+
+    @GetMapping(value = "userManage/check")
+    public Result checkToken(@RequestHeader("token") String token);
 }
