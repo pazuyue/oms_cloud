1a81bb1331a4ac60ac03be3ea536bae25eb31ca9
yueguang <yueguang@qingmutec.com>
2023-12-11 13:55:33 +0800

    按照对照关系，切换库

--- oms_commodity/src/main/java/com/oms/saas/commodity/Filter/DynamicDatasourceInterceptorFilter.java
+++ oms_commodity/src/main/java/com/oms/saas/commodity/Filter/DynamicDatasourceInterceptorFilter.java
@@ -1,12 +1,14 @@
 package com.oms.saas.commodity.Filter;
 
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import com.oms.saas.commodity.dto.JwtInfo;
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
 
@@ -14,11 +16,23 @@ import java.io.IOException;
 @Component // 注册拦截器
 public class DynamicDatasourceInterceptorFilter implements Filter {
 
+    @Resource
+    private JwtInfo jwtInfo;
+    @Value("${databaseName:false}")
+    private String databaseName;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         System.out.println("===> DynamicDatasourceInterceptor doFilter");
         HttpServletRequest req = (HttpServletRequest)servletRequest;
         HttpServletResponse resp = (HttpServletResponse)servletResponse;
+        String companyCode = jwtInfo.getCompanyCode().toLowerCase();
+        databaseName = databaseName.toLowerCase();
+        String dataKey =companyCode+"_"+databaseName;
+        System.out.println("dataKey:"+dataKey);
+        DynamicDataSourceContextHolder.clear();
+        //切换到对应poolName的数据源
+        DynamicDataSourceContextHolder.push(dataKey);
         chain.doFilter(req,resp);
     }
 }
