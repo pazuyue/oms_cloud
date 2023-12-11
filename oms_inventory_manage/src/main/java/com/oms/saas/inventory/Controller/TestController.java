f00f96a874fb9357497a68a262b392d7ceacc02b
yueguang <yueguang@qingmutec.com>
2023-12-11 12:02:32 +0800

    按照对照关系，切换库

--- oms_inventory_manage/src/main/java/com/oms/saas/inventory/Controller/TestController.java
+++ oms_inventory_manage/src/main/java/com/oms/saas/inventory/Controller/TestController.java
@@ -1,6 +1,10 @@
 package com.oms.saas.inventory.Controller;
 
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
 import com.oms.saas.inventory.api.Result;
+import com.oms.saas.inventory.dto.jwt.JwtInfo;
+import com.oms.saas.inventory.until.DynamicRoutingDataSource;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,10 @@ public class TestController {
 
     @Value("${useLocalCache:false}")
     private boolean useLocalCache;
+    @Resource
+    private JwtInfo jwtInfo;
+    @Value("${databaseName:false}")
+    private String databaseName;
 
     @RequestMapping(value = "/get", method = GET)
     @ResponseBody
@@ -24,6 +32,21 @@ public class TestController {
     @GetMapping("/echo/{string}")
     public Result echo(@PathVariable String string) {
         return Result.success("Hello," + string);
+    }
+
+    @GetMapping("/testSwitchDataSource")
+    public Result testSwitchDataSource(){
+        String companyCode = jwtInfo.getCompanyCode().toLowerCase();
+        System.out.println("companyCode:"+companyCode);
+        databaseName = databaseName.toLowerCase();
+        String dataKey =companyCode+"_"+databaseName;
+        System.out.println("dataKey:"+dataKey);
+        //需要注意的是手动切换的数据源，最好自己在合适的位置
+        //调用DynamicDataSourceContextHolder.clear()清空当前线程的数据源信息。
+        DynamicDataSourceContextHolder.clear();
+        //切换到对应poolName的数据源
+        DynamicDataSourceContextHolder.push(dataKey);
+        return Result.success();
     }
 
 }
--- oms_inventory_manage/src/main/java/com/oms/saas/inventory/Filter/DynamicDatasourceInterceptorFilter.java
+++ oms_inventory_manage/src/main/java/com/oms/saas/inventory/Filter/DynamicDatasourceInterceptorFilter.java
@@ -1,22 +1,39 @@
 package com.oms.saas.inventory.Filter;
 
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import com.oms.saas.inventory.dto.jwt.JwtInfo;
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.sql.ShardingKey;
 
 @Order(10)
 @Component // 注册拦截器
 public class DynamicDatasourceInterceptorFilter implements Filter {
+
+    @Resource
+    private JwtInfo jwtInfo;
+    @Value("${databaseName:false}")
+    private String databaseName;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         System.out.println("===> DynamicDatasourceInterceptor doFilter");
         HttpServletRequest req = (HttpServletRequest)servletRequest;
         HttpServletResponse resp = (HttpServletResponse)servletResponse;
+        String companyCode = jwtInfo.getCompanyCode().toLowerCase();
+        databaseName = databaseName.toLowerCase();
+        String dataKey =companyCode+"_"+databaseName;
+        System.out.println("dataKey:"+dataKey);
+        DynamicDataSourceContextHolder.clear();
+        //切换到对应poolName的数据源
+        DynamicDataSourceContextHolder.push(dataKey);
         chain.doFilter(req,resp);
     }
 }
