ea34cd9b50c0426a4ddb971976ed5ac83cfd2f71
yueguang <yueguang@qingmutec.com>
2024-01-05 18:17:35 +0800

    优雅关机

--- saas_common_security/src/main/java/com/saas/common/security/CommonSecurityApplication.java
+++ saas_common_security/src/main/java/com/saas/common/security/CommonSecurityApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -13,6 +17,23 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CommonSecurityApplication {
 
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CommonSecurityApplication.class);
+        app.setRegisterShutdownHook(true); // 自动注册关闭钩子
         SpringApplication.run(CommonSecurityApplication.class, args);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                // 执行优雅停机逻辑
+                System.out.println("执行优雅停机逻辑");
+                executor.shutdown(); // 关闭线程池
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    executor.shutdownNow(); // 强制关闭线程池
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow(); // 强制关闭线程池
+            }
+        }));
+
     }
 }
